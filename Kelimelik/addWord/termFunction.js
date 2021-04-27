a4e7c34c660d87120e61c8b646ad1f92c8eb54d3
Burak Yeşilyurt <76653369+burakyesilyurt@users.noreply.github.com>
2021-04-27 13:04:23 +0300

    Update termFunction.js

--- Kelimelik/addWord/termFunction.js
+++ Kelimelik/addWord/termFunction.js
@@ -7,22 +7,12 @@ words.push({
   english:"Please click the Add Word for adding a new word",
   turkish:"Lütfen yeni kelime eklemek için Add Word'e basın "
 })
-let termsInForms = {
-  turkish : "",
-  english :""
-}
+
 let termJSON = localStorage.getItem("words")
 if(termJSON !== null){
   words = JSON.parse(termJSON)
 }
 
-document.querySelector("#english").addEventListener("input", function(e){
-  termsInForms.english = e.target.value
-})
-
-document.querySelector("#turkish").addEventListener("input",function(e){
-  termsInForms.turkish = e.target.value
-})
 
 document.querySelector("#inputSelector").addEventListener("submit", function(e){
   e.preventDefault()
